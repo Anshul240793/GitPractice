5023414fc4ef37939d95e4d027087f4c7309d77f
anshultekleads <anshul2407@gmail.com>
2020-05-11 15:47:33 +0530

    In Test class main method is added

--- TimeProject/src/com/nt/test/Test.java
+++ TimeProject/src/com/nt/test/Test.java
@@ -1,5 +1,11 @@
 package com.nt.test;
 
+import com.nt.beans.UtilJava;
+
 public class Test {
+	public static void main(String[] args) {
+		UtilJava ut=new UtilJava();
+		System.out.println(ut.getSysDate());
+	}
 
 }
